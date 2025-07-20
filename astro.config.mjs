303ceda2da888d6f7fb012068b9c1eb6665166a8
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-20 16:44:17 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
   integrations: [icon()],
+  adapter: vercel(),
 });
