76cd8d942a71056fb9d67d7623cd54c50924b089
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-20 19:22:50 +0200

    rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,11 +12,6 @@ export default defineConfig({
 
   vite: {
       plugins: [tailwindcss()],
-      resolve: {
-      alias: {
-        '@': new URL('./src', import.meta.url).pathname
-      }
-    }
     },
 
   integrations: [icon()],
