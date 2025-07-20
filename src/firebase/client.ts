c61caaf4ad9a80f35893b684a615dd4aa38eedb8
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-20 19:03:50 +0200

    Update client.ts

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAGeTp4WQFP1mcDreHThX7w6G-YgF8pVE",
-  authDomain: "journey-4d59b.firebaseapp.com",
-  projectId: "journey-4d59b",
-  storageBucket: "journey-4d59b.firebasestorage.app",
-  messagingSenderId: "487803690897",
-  appId: "1:487803690897:web:5722b4c910753040b6c430",
-  measurementId: "G-1H00LW82M8"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
