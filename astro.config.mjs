b03eb6bbd1bfd0612e3a7fea12491f3675a8f1f9
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-20 20:24:26 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
