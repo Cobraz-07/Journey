9d17d132b9cc3b454db2f8e411c499a7e556199a
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-20 18:54:39 +0200

    Add auth
    Will add corresponding pages

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAGeTp4WQFP1mcDreHThX7w6G-YgF8pVE",
+  authDomain: "journey-4d59b.firebaseapp.com",
+  projectId: "journey-4d59b",
+  storageBucket: "journey-4d59b.firebasestorage.app",
+  messagingSenderId: "487803690897",
+  appId: "1:487803690897:web:5722b4c910753040b6c430",
+  measurementId: "G-1H00LW82M8"
+};
+
+export const app = initializeApp(firebaseConfig);
