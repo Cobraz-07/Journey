116db76b2874712c657a9f78a310c072f9eacd94
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2025-08-26 02:09:44 +0200

    format again

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,17 @@ export const onRequest = defineMiddleware((context, next) => {
     const currentUser = projectAuth.currentUser;
     const { pathname } = context.url;
 
-    if(!currentUser && pathname.startsWith("/authenticated") && context.request.method === "GET"){
-        return context.redirect("/register")
+    if (!currentUser && pathname.startsWith("/authenticated") && context.request.method === "GET") {
+        return context.redirect("/register");
     }
 
-    if(currentUser && (pathname === "/signin" || pathname === "/register" || pathname === "/")){
-        return context.redirect("/authenticated/")
+    if (currentUser && (pathname === "/signin" || pathname === "/register" || pathname === "/")) {
+        return context.redirect("/authenticated/");
     }
 
-    if(currentUser){
+    if (currentUser) {
         context.locals.userEmail = currentUser.email;
     }
-    
-    return next()
-})
+
+    return next();
+});
