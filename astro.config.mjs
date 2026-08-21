45fd82dec542f5d393afe698323122e65aa13043
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2026-08-21 14:15:31 +0200

    perf: autoalojar fuente variable plus-jakarta-sans y optimizar carga para eliminar recursos que bloquean el renderizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,14 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  compressHTML: true,
   integrations: [icon()],
   adapter: vercel(),
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: true,
+      cssMinify: true,
     },
+  },
 });
